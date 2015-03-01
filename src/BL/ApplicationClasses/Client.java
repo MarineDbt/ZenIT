ce80cd2c79b7ce5561ffbd6c4b0969000b3263f9
Elie <Elie.gallet@gmail.com>
2015-03-01 23:58:22 +0100

    yo

--- src/BL/ApplicationClasses/Client.java
+++ src/BL/ApplicationClasses/Client.java
@@ -1,5 +1,15 @@
 package BL.ApplicationClasses;
 
+import UI.ClientUI;
+import BL.Front.UserFacade;
+
 public class Client {
 
+	UserFacade userFacade;
+	ClientUI clientUI;
+	
+	public Client(){
+		
+	}
+	
 }
---
+++ src/BL/Front/UserFacade.java
@@ -0,0 +1,41 @@
+
+package BL.Front;
+
+import BL.DataModel.User;
+import DataBase.ConnectionToBD;
+
+/**
+ * 
+ * 
+ */
+public class UserFacade {
+	
+	User currentUser;
+	
+	public void login(String id, String psw){
+		 ConnectionToBD connect=new ConnectionToBD();
+		 try {
+			if (connect.checkPassword(id,psw)){
+				 this.loadData();
+			 }
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	 }
+	 public void register(String firstname, String lastname, String street, int PC, String city, String phone, String email, String id, String psw){
+		 ConnectionToBD connect=new ConnectionToBD();
+		 try{
+		 connect.insertUser(firstname, lastname, street, PC, city, phone, email, id, psw);
+		 }
+		 catch (Exception e){
+			 e.printStackTrace();
+		 }
+		 
+	 }
+	 
+	 //charge la persistance
+	 public void loadData(){
+		 //TODO
+	 }
+ }
