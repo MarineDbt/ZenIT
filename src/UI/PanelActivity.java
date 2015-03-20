69e53651ae9747992c150ee41584acadd2d7eb41
Marine <Marine@>
2015-03-20 10:55:20 +0100

    Jolies couleurs

--- src/UI/PanelActivity.java
+++ src/UI/PanelActivity.java
@@ -2,8 +2,11 @@ package UI;
 
 import javax.swing.JPanel;
 import javax.swing.JLabel;
+
 import java.awt.Font;
+
 import javax.swing.JButton;
+import java.awt.Color;
 
 public class PanelActivity extends JPanel {
 
@@ -11,13 +14,29 @@ public class PanelActivity extends JPanel {
 	 * Create the panel.
 	 */
 	public PanelActivity() {
+		setBackground(Color.ORANGE);
+	
 		setLayout(null);
 		
 		JButton btnAjouterUneActivit = new JButton("Ajouter une activit\u00E9");
 		btnAjouterUneActivit.setBounds(256, 11, 146, 23);
 		add(btnAjouterUneActivit);
 		
+		JPanel panel = new JPanel();
+		panel.setBounds(10, 40, 639, 253);
+		add(panel);
+		
+		
+		/* int nbBtn = 100;
+		JButton[] btn;
+		btn = new JButton[nbBtn];
+		for (int i=0; i<btn.length; i++){
+			JButton bouton = new JButton("Activité "+Integer.toString(i));
+			btn[i]=bouton;
+			panel.add(bouton); }
+		*/
+
+	
 
 	}
-
 }
--- src/UI/UISupervisor.java
+++ src/UI/UISupervisor.java
@@ -13,6 +13,7 @@ import java.awt.Font;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.Color;
 
 public class UISupervisor extends JFrame implements ActionListener {
 
@@ -22,16 +23,20 @@ public class UISupervisor extends JFrame implements ActionListener {
 	 * Create the frame.
 	 */
 	public UISupervisor(AbstractPersistenceHandlerFactory factory) {
+		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 695, 500);
 		contentPane = new JPanel();
+		contentPane.setBackground(Color.PINK);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
+		
 		JPanel panel = new JPanel();
-		panel.setBounds(10, 11, 659, 97);
+		panel.setBounds(10, 11, 669, 91);
 		contentPane.add(panel);
+	
 		panel.setLayout(null);
 		
 		JLabel lblZenLounge = new JLabel("Zen Lounge");
@@ -51,53 +56,42 @@ public class UISupervisor extends JFrame implements ActionListener {
 		
 		JButton btnNewButton = new JButton("Notifications");
 		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 9));
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-			}
-		});
 		btnNewButton.setBounds(180, 63, 92, 23);
 		panel.add(btnNewButton);
 		
 		JButton button = new JButton("Mes activités");
-		button.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
+		button.addActionListener(this);
+		button.setActionCommand("activity");	
 		button.setFont(new Font("Tahoma", Font.PLAIN, 9));
 		button.setBounds(282, 63, 92, 23);
 		panel.add(button);
 		
 		JButton button_1 = new JButton("Magasin");
-		button_1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
 		button_1.setFont(new Font("Tahoma", Font.PLAIN, 9));
 		button_1.setBounds(384, 63, 76, 23);
 		panel.add(button_1);
 		
 		JButton button_2 = new JButton("Panier");
-		button_2.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
 		button_2.setFont(new Font("Tahoma", Font.PLAIN, 9));
 		button_2.setBounds(470, 63, 76, 23);
 		panel.add(button_2);
 		
 		JButton button_3 = new JButton("Deconnexion");
-		button_3.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		button_3.setFont(new Font("Tahoma", Font.PLAIN, 9));
-		button_3.setBounds(556, 63, 93, 23);
-		panel.add(button_3);
+		
+			button_3.setFont(new Font("Tahoma", Font.PLAIN, 9));
+			button_3.setBounds(556, 63, 93, 23);
+			panel.add(button_3);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
+		if (e.getActionCommand().equals("activity")) {
+			PanelActivity activityPanel = new PanelActivity();
+			activityPanel.setBounds(10, 110, 659, 300);
+			this.contentPane.add(activityPanel);
+			this.contentPane.repaint();
+		}
 		
 	}
 }
